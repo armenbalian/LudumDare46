b3cac39e0b22b230742654ff070ba5fae24fdda2
Raphael Fortin <raphaelfortin@hotmail.com>
2020-04-17 23:08:50 -0400

    Character GroundSensor

---
+++ LudumDare46/Assets/Scripts/GroundSensor.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundSensor : MonoBehaviour
+{
+    MeshRenderer meshRenderer;
+
+    Dictionary<string, bool> allGroundsStatus;
+
+    private void Awake()
+    {
+        allGroundsStatus = new Dictionary<string, bool>();
+        var grounds = FindObjectsOfType<GameObject>().Where(g => g.layer == LayerMask.NameToLayer("Ground"));
+        foreach (var ground in grounds)
+        {
+            allGroundsStatus.Add(ground.name, false);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        meshRenderer.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public bool GetIsTouchingGround()
+    {
+        return allGroundsStatus.Any(c => c.Value);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var gameObject = other.gameObject;
+        if (gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            allGroundsStatus[gameObject.name] = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        var gameObject = other.gameObject;
+        if (gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            allGroundsStatus[gameObject.name] = false;
+        }
+    }
+}
---
+++ LudumDare46/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 3.0f;
+    [SerializeField]
+    private float gravity = -19.62f;
+    [SerializeField]
+    private float jumpHeight = 1f;
+    [SerializeField]
+    private bool isGrounded = false;
+
+    GroundSensor groundSensor;
+    CharacterController controller;
+
+    Vector2 velocity;
+
+    private void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+        if (controller == null)
+            throw new System.Exception("Player Movement - Need CharacterController");
+
+        groundSensor = GetComponentInChildren<GroundSensor>();
+        if (groundSensor == null)
+            throw new System.Exception("Player Movement - Need GroundSensor");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = groundSensor.GetIsTouchingGround();
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float xMoveInput = Input.GetAxis("Horizontal");
+        Move(xMoveInput);
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            Jump();
+        }
+    }
+
+    void Move(float xMoveInput)
+    {
+        if (xMoveInput != 0)
+        {
+            Vector2 move = new Vector2(xMoveInput, 0.0f);
+            controller.Move(move * speed * Time.deltaTime);
+        }
+    }
+
+    void Jump()
+    {
+        velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
