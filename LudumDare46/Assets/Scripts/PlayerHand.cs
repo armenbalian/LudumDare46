e2c3f4d623239651415a03724ada1de1e2bfe648
Raphael Fortin <raphaelfortin@hotmail.com>
2020-04-18 23:56:20 -0400

    PlayerHand + gun fix

---
+++ LudumDare46/Assets/Scripts/PlayerHand.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHand : MonoBehaviour
+{
+    [SerializeField]
+    Gun gun;
+
+    private void Awake()
+    {
+        if (gun == null)
+            throw new System.Exception("Player Hand - Need Gun !");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var newPos = transform.position;
+        newPos.x += 0.2f;
+        newPos.y += 0.15f;
+        gun.transform.position = newPos;
+    }
+}
