2a5f4387a734a4fbf5a8226062acaeeab116380d
Raphael Fortin <raphaelfortin@hotmail.com>
2020-04-19 15:42:59 -0400

    background

---
+++ LudumDare46/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField]
+    Transform target;
+
+    Vector3 offset;
+
+    void Awake()
+    {
+        offset = transform.position - target.position;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = new Vector3(target.position.x + offset.x, transform.position.y, transform.position.z);
+    }
+}
